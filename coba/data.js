6da5db910e785e4aa8f3d4baa118d7d85a28308f
nakha-mub <webdev.nakhamub@gmail.com>
2025-05-05 21:07:42 +0800

    Add files via upload

---
+++ coba/data.js
@@ -0,0 +1,51 @@
+fetch('DataSiswa.txt')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Gagal mengambil data');
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log = data;
+      })
+      .catch(error => {
+        console.log = 'Terjadi kesalahan: ' + error.message;
+      });
+
+const rawData = data;
+
+
+/*const rawData = `
+XII IPA 1	22231001	78687146	A. ARYA INDRA RABBANI	L	SEPAK BOLA	LULUS
+XII IPA 1	22231013	68864284	ACHMAD HAMZAH	L	KARATE	LULUS
+`; // Tempelkan seluruh isi file di sini
+*/
+
+const students = rawData.trim().split('\n').map(line => {
+  const [kelas, nis, nisn, nama, jk, cabor, status] = line.split('	');
+  return { kelas, nis, nisn, nama, jk, cabor, status };
+});
+
+console.log(students);
+
+
+/*const students = [
+  {
+    nisn: "0041957865",
+	nis: "242500212",
+    nama: "Andi Prasetyo",
+    kelas: "XII IPA 1",
+	jk: "Laki-laki",
+	cabor: "Bulutangkis",
+    status: "Lulus"
+  },
+  {
+    nisn: "0041957866",
+	nis: "242500212",
+    nama: "Budi Santoso",
+    kelas: "XII IPA 2",
+	jk: "Laki-laki",
+	cabor: "Sepakbola",
+    status: "Tidak Lulus"
+  }
+];*/
